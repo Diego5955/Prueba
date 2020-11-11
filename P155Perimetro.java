b0e098cee14543d109980b0926e44aae35890301
Joaquin0-0 <jopego02@xuqueralumnat.es>
2020-11-11 10:33:27 +0100

    Primer commit 2

---
+++ P155Perimetro.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class P155Perimetro {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+
+		int n1;
+		int n2;
+		
+		n1= teclado.nextInt();
+		n2 = teclado.nextInt();
+		
+	while ((n1>=0) && (n2>=0)) {}
+	}
+
+}
